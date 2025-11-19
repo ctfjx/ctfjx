5d065f21cca90ea9c82aa76a4536486021e94e8b
AlexNg <contact@ngjx.org>
2025-11-19 19:05:38 +0800

    feat: Move stream flags to bitflags
    Signed-off-by: AlexNg <contact@ngjx.org>

--- crates/mux/src/stream/mod.rs
+++ crates/mux/src/stream/mod.rs
@@ -1,78 +1,101 @@
 use crate::{StreamId, error::Error, frame::Frame};
+use bitflags::bitflags;
+use parking_lot::RwLock;
 use std::{
     cmp,
     pin::Pin,
-    sync::{
-        OnceLock,
-        atomic::{AtomicBool, Ordering},
-    },
+    sync::OnceLock,
     task::{Context, Poll},
 };
 use tokio::{
     io::{AsyncRead, AsyncWrite, ReadBuf},
-    sync::{broadcast, mpsc, oneshot},
+    sync::{mpsc, oneshot},
 };
 use tokio_util::bytes::{Buf, Bytes};
 
+pub(crate) mod allocate;
+pub(crate) use allocate::*;
 pub(crate) mod message;
 pub(crate) use message::*;
 
 type FrameWriteFuture = Pin<Box<dyn Future<Output = Result<usize, Error>> + Send + Sync>>;
 
+bitflags! {
+    pub struct StreamPerms: u8 {
+        const R = 1 << 0;
+        const W = 1 << 1;
+        const RW = Self::R.bits() | Self::W.bits();
+    }
+}
+
+// closing:
+// A -> FIN -> peer
+// A denies w
+// peer denies rw
+// peer -> FIN -> A
+// A denies rw
 pub struct Stream {
     stream_id: StreamId,
+    perms: RwLock<StreamPerms>,
 
     in_rx: mpsc::UnboundedReceiver<Frame>,
-    read_open: AtomicBool, // assume if read=false, stream is closed
     read_buf: Bytes,
 
     out_tx: mpsc::UnboundedSender<Message>,
-    write_open: AtomicBool,
     current_write_future: Option<FrameWriteFuture>,
 
+    // for stream to internally send out when we loose rw perms
+    trigger_close_tx: mpsc::UnboundedSender<StreamId>,
+    // listen into when peer sends FIN
     peer_close_rx: oneshot::Receiver<()>,
-
-    shutdown_rx: broadcast::Receiver<()>, // triggered when raw conn is closed
     close_once: OnceLock<()>,
 }
 
 impl Stream {
-    pub fn new(
+    pub(crate) fn new(
         stream_id: StreamId,
         in_rx: mpsc::UnboundedReceiver<Frame>,
         out_tx: mpsc::UnboundedSender<Message>,
+        trigger_close_tx: mpsc::UnboundedSender<StreamId>,
         peer_close_rx: oneshot::Receiver<()>,
-        shutdown_rx: broadcast::Receiver<()>,
     ) -> Self {
         Self {
             stream_id,
+            perms: RwLock::new(StreamPerms::RW),
             read_buf: Bytes::new(),
             current_write_future: None,
             in_rx,
             out_tx,
-            read_open: AtomicBool::new(true),
-            write_open: AtomicBool::new(true),
-            shutdown_rx,
+            trigger_close_tx,
             peer_close_rx,
             close_once: OnceLock::new(),
         }
     }
 
+    // Should be used to send out a FIN
     pub fn close(&self) {
         self.close_once.get_or_init(|| {
-            self.write_open.store(false, Ordering::Release);
+            self.deny_perm(StreamPerms::W);
             let _ = message::send_fin(self.out_tx.clone(), self.stream_id);
         });
+    }
+
+    pub fn deny_perm(&self, perm: StreamPerms) {
+        let mut p = self.perms.write();
+        *p -= perm & StreamPerms::RW;
+
+        if !p.contains(StreamPerms::RW) {
+            let _ = self.trigger_close_tx.send(self.stream_id);
+        }
     }
 }
 
 impl Drop for Stream {
     fn drop(&mut self) {
-        if self.write_open.load(Ordering::Acquire) {
+        if self.perms.read().contains(StreamPerms::W) {
             let _ = message::send_fin(self.out_tx.clone(), self.stream_id);
         }
-        self.write_open.store(false, Ordering::Release);
-        self.read_open.store(false, Ordering::Release);
+        self.deny_perm(StreamPerms::RW);
     }
 }
 
@@ -85,7 +108,7 @@ impl AsyncRead for Stream {
         let self_mut = self.get_mut();
 
         loop {
-            if !self_mut.read_open.load(Ordering::Acquire) {
+            if !self_mut.perms.read().contains(StreamPerms::R) {
                 return Poll::Ready(Err(std::io::Error::new(
                     std::io::ErrorKind::BrokenPipe,
                     "stream is closed",
@@ -111,7 +134,7 @@ impl AsyncRead for Stream {
                 Poll::Pending => {
                     match Pin::new(&mut self_mut.peer_close_rx).poll(cx) {
                         Poll::Ready(_) => {
-                            self_mut.read_open.store(false, Ordering::Release);
+                            self_mut.deny_perm(StreamPerms::R);
                             return Poll::Ready(Ok(()));
                         }
                         Poll::Pending => (),
@@ -129,7 +152,7 @@ impl AsyncWrite for Stream {
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, std::io::Error>> {
-        if !self.write_open.load(Ordering::Acquire) {
+        if !self.perms.read().contains(StreamPerms::W) {
             return Poll::Ready(Err(std::io::Error::new(
                 std::io::ErrorKind::BrokenPipe,
                 "stream is closed for writing",
