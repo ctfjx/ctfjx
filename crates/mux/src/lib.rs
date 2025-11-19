86091b5ec7a6c601094820a785feaa6e64e2b7ef
AlexNg <contact@ngjx.org>
2025-11-19 19:07:41 +0800

    feat: Add multiplexer
    Signed-off-by: AlexNg <contact@ngjx.org>

--- crates/mux/src/lib.rs
+++ crates/mux/src/lib.rs
@@ -1,8 +1,10 @@
 pub(crate) mod consts;
 pub(crate) mod frame;
+pub(crate) mod multiplexer;
 pub(crate) mod stream;
 
 pub(crate) use consts::*;
 
 pub mod error;
+pub use multiplexer::Multiplexer;
 pub use stream::Stream;
